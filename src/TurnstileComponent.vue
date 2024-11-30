711b1f852bb53af053edc7eacdb2e51ab336e7fa
Astrian Zheng <happy.wheel6743@astrian.moe>
2024-11-30 18:00:47 +1100

    update: bump version to 2.0.0 and enhance TurnstileComponent props and callbacks

--- src/TurnstileComponent.vue
+++ src/TurnstileComponent.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, onMounted, PropType } from 'vue'
 
 export default defineComponent({
   name: "TurnstileComponent",
@@ -7,15 +7,81 @@ export default defineComponent({
     sitekey: {
       type: String,
       required: true
+    },
+    action: {
+      type: String,
+      required: false
+    },
+    cData: {
+      type: String,
+      required: false
+    },
+    execution: {
+      type: String as PropType<"render" | "execute">,
+      required: false
+    },
+    theme: {
+      type: String as PropType<"light" | "dark" | "auto">,
+      required: false
+    },
+    language: {
+      type: String,
+      required: false
+    },
+    tabindex: {
+      type: Number,
+      required: false
+    },
+    responseField: {
+      type: String,
+      required: false
+    },
+    responseFieldName: {
+      type: String,
+      required: false
+    },
+    size: {
+      type: String as PropType<"normal" | "flexible" | "compact">,
+      required: false
+    },
+    retry: {
+      type: String as PropType<"auto" | "never">,
+      required: false
+    },
+    retryInterval: {
+      type: Number,
+      required: false
+    },
+    refreshExpired: {
+      type: String as PropType<"auto" | "manual" | "never">
+    },
+    refreshTimeout: {
+      type: String as PropType<"auto" | "manual" | "never">,
+      required: false
+    },
+    appearance: {
+      type: String as PropType<"always" | "execute" | "interaction-only">,
+      required: false
+    },
+    feedbackEnabled: {
+      type: Boolean,
+      required: false
     }
   },
   emits: {
-    verify: (response: string) => {
-        if (response !== null && response !== "") return true
-        else return false
+    callback: (response: string) => {
+      if (response !== null && response !== "") return true
+      else return false
     },
-    expire: null,
-    fail: null
+    errorCallback: (response: string) => {
+      if (response !== null && response !== "") return true
+      else return false
+    },
+    expiredCallback: null,
+    beforeInteractiveCallback: null,
+    afterInteractiveCallback: null,
+    unsupportedCallback: null,
+    timeoutCallback: null
   },
   setup(props, context) {
     // const turnstileBox = ref(null)
@@ -34,9 +100,28 @@ export default defineComponent({
       window.onloadTurnstileCallback = () => {
         window.turnstile?.render("#turnstile-box", {
           sitekey: props.sitekey,
-          callback: (response: string) => context.emit('verify', response),
-          'expired-callback': context.emit('expire'),
-          'error-callback': context.emit('fail')
+          callback: (response: string) => context.emit('callback', response),
+          'expired-callback': () => context.emit('expiredCallback'),
+          'error-callback': (response: string) => context.emit('errorCallback', response),
+          action: props.action,
+          cData: props.cData,
+          execution: props.execution ?? 'render',
+          "before-interactive-callback": () => context.emit('beforeInteractiveCallback'),
+          "after-interactive-callback": () => context.emit('afterInteractiveCallback'),
+          "unsupported-callback": () => context.emit('unsupportedCallback'),
+          theme: props.theme ?? 'auto',
+          language: props.language ?? 'auto',
+          tabindex: props.tabindex ?? 0,
+          "timeout-callback": () => context.emit('timeoutCallback'),
+          "response-field": props.responseField,
+          "response-field-name": props.responseFieldName,
+          size: props.size,
+          retry: props.retry,
+          "retry-interval": props.retryInterval,
+          "refresh-expired": props.refreshExpired,
+          "refresh-timeout": props.refreshTimeout,
+          apperance: props.appearance,
+          "feedback-enabled": props.feedbackEnabled
         })
       }
     }
